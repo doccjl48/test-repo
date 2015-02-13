a9c83cbc60ea3db2b821372f3d05fe195edb9bfd
doccjl48 <doccjl48@gmail.com>
2015-02-13 16:20:54 -0600

    This is revised

---
+++ pollutantmean2.R
@@ -0,0 +1,16 @@
+
+## polutant mean
+pollutantmean<-function(directory, pollutant, id = 1:332){
+       
+        filesList<-list.files(directory,full.names=TRUE)
+        dat<-data.frame()
+        
+        for(i in id){
+                dat<-rbind(dat,read.csv(filesList[i]))
+                
+        }
+
+        
+        mean(dat[ ,pollutant], na.rm=TRUE)
+        
+}
